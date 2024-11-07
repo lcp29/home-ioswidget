7811fa4dd4bf334423a45b141ccd66668119e82a
helmholtz <helmholtz@fomal.host>
2024-11-07 13:53:08 +0800

    Initial Commit

---
+++ HA Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HA Assistant
+//
+//  Created by 李昌埔 on 2024/11/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HA Assistant/HA_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  HA_AssistantApp.swift
+//  HA Assistant
+//
+//  Created by 李昌埔 on 2024/11/7.
+//
+
+import SwiftUI
+
+@main
+struct HA_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
